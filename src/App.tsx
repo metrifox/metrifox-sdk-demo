dd152698fae92260edaccc3fe6c5039df503b10c
Samuel Ogunleye <61986442+Sproff@users.noreply.github.com>
2026-01-27 05:05:35 -0800

    fix(widget): add missing Plan Card configuration and expand labels (#7)
    * fix(widget): add missing Plan Card configuration and expand labels
    
    * chore: update react-sdk version and docs
    
    * chore: restructure files and cleanup root directory

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import { widgetCatalog } from "./utils/widget" // Import widget catalog
-import { PlaygroundLayout } from "./components/playground/layout"
-
-const App = () => {
-  return <PlaygroundLayout widgets={widgetCatalog} />
-}
-
-export default App
